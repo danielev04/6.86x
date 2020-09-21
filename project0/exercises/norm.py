6ddad6d82028ad874409b87ed32fe33de8bd3d5a
danielev04 <daniele@vottero.eu>
2020-09-21 16:06:11 +0100

    Create norm.py

---
+++ project0/exercises/norm.py
@@ -0,0 +1,20 @@
+import numpy as np
+
+def norm(A, B):
+    """
+    Takes two Numpy column arrays, A and B, and returns the L2 norm of their
+    sum.
+
+    Arg:
+      A - a Numpy array
+      B - a Numpy array
+    Returns:
+      s - the L2 norm of A+B.
+    """
+    #Your code here
+    s = np.linalg.norm(A+B)
+    return s
+
+result = norm(2,3)
+print(result)
+
